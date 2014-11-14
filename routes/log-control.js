454344ac20aa8003e1dd036f9f9a005044a99e06
John Felten <john_felten@yahoo.com>
2014-11-14 01:20:16 -0800

    comment log file

--- routes/log-control.js
+++ routes/log-control.js
@@ -12,7 +12,7 @@ logger.setLevels({debug:0,info: 1,silly:2,warn: 3,error:4,});
 logger.addColors({debug: 'green',info:  'cyan',silly: 'magenta',warn:  'yellow',error: 'red'});
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, { level: 'debug', colorize:true });
-logger.add(logger.transports.File, { filename: fileName });
+//logger.add(logger.transports.File, { filename: fileName });
 exports.logger = logger;
 
 LoggerControl = function(io) {
