3ed3a0b1aaf67b932cec415a7d175f0b8b798b1e
knowhow <john@zenzic.com>
2015-07-03 15:49:53 -0700

    add port option

--- runServer.js
+++ runServer.js
@@ -1,12 +1,20 @@
 var respawn = require('respawn')
+var serverData = require('yargs').argv;
 
-var monitor = respawn(['node', 'index.js'], {
+var port="3002";
+
+if (serverData.port) {
+	port = serverData.port;
+}
+
+var monitor = respawn(['node', 'index.js', '--port='+port], {
   env: {ENV_VAR:'test'}, // set env vars
   cwd: '.',              // set cwd
-  maxRestarts: -1,        // how many restarts are allowed within 60s
+  maxRestarts: 10,        // how many restarts are allowed within 60s
                          // or -1 for infinite restarts
   sleep:1000,            // time to sleep between restarts,
   kill:30000            // wait 30s before force killing after stopping
+
 });
 
 var buf = [];
